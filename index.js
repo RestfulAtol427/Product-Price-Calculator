81802e7f19e1f693a43e69c777b211048f15e77a
cbyers00 <cbyers@nagios.com>
2023-04-12 16:02:50 -0500

    Made togglable table with onlick event

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ function closeWindow() {
   document.getElementById("myOverlay").style.display = "none";
 }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function displayOffice() {
+  var coll = document.getElementsByClassName("collapsibleOffice");
+  var i;
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 }

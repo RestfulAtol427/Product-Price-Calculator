18dd1f6ae303fc34e7258e57f625d3a9a9ba632c
cbyers00 <cbyers@nagios.com>
2023-04-12 15:49:34 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// Open the full screen search box
+function openWindow() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeWindow() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}

06e0e795161e2f95a2805079a00d7d1dd904b845
ATMcrazy8 <austin.t.mans@gmail.com>
2023-04-13 13:23:28 -0500

    updated with Austins code

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+// Make the Checkbox for XI toggle the Calculator display
+
+const checkbox = document.getElementById('xiCheckbox');
+const div = document.getElementById('xiCalculator');
+
+if (checkbox.checked) {
+  div.style.display = 'block';
+}
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    div.style.display = 'block';
+  } else {
+    div.style.display = 'none';
+  }
+});
+
+
+
 
 // Open the full screen search box
 function openWindow() {
-  document.getElementById("myOverlay").style.display = "block";
+  document.getElementById("xiTableOverlay").style.display = "inline-block";
 }
+
+
+
+
+
 
 // Close the full screen search box
 function closeWindow() {
-  document.getElementById("myOverlay").style.display = "none";
+  document.getElementById("xiTableOverlay").style.display = "none";
 }
 
-function displayOffice() {
-  var coll = document.getElementsByClassName("collapsibleOffice");
-  var i;
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
+
+
+
+
+
+//table/sub-table js
+function toggleTable(row) {
+  var subTable = row.nextElementSibling;
+  if (subTable.style.display === "none") {
+    subTable.style.display = "table-row";
+  } else {
+    subTable.style.display = "none";
   }
 }

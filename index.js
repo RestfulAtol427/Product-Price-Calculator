b7413ba5d5a05c29b554838b1a318cfc6ef54f9a
ATMcrazy8 <austin.t.mans@gmail.com>
2023-04-13 17:05:43 -0500

    Killed old Calc. Building new calc with mngr input

--- index.js
+++ index.js
@@ -1,49 +1,170 @@
-// Make the Checkbox for XI toggle the Calculator display
+//Linux Server Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
 
-const checkbox = document.getElementById('xiCheckbox');
-const div = document.getElementById('xiCalculator');
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
 
-if (checkbox.checked) {
-  div.style.display = 'block';
-}
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
 
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    div.style.display = 'block';
-  } else {
-    div.style.display = 'none';
-  }
-});
+  const linuxServerProduct = num01 * num02;
 
-
-
-
-// Open the full screen search box
-function openWindow() {
-  document.getElementById("xiTableOverlay").style.display = "inline-block";
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
 }
 
 
+//Windows Server Calcs
+const num03 = parseInt();
+const num04 = parseInt();
+const windowsServerProduct = num03 + num04;
 
+console.log(`The product of ${num03} and ${num04} is ${windowsServerProduct}`);
 
+function calculateProduct() {
+  const num03 = parseInt(document.getElementById('num03').value);
+  const num04 = parseInt(document.getElementById('num04').value);
 
+  const windowsServerProduct = num03 * num04;
 
-// Close the full screen search box
-function closeWindow() {
-  document.getElementById("xiTableOverlay").style.display = "none";
+  document.getElementById('resultWindowsServer').textContent = `Services: ${windowsServerProduct}`;
 }
 
 
+//Databases Calcs
+const num05 = parseInt();
+const num06 = parseInt();
+const databaseProduct = num05 + num06;
 
+console.log(`The product of ${num05} and ${num06} is ${databaseProduct}`);
 
+function calculateProduct() {
+  const num05 = parseInt(document.getElementById('num05').value);
+  const num06 = parseInt(document.getElementById('num06').value);
 
+  const databaseProduct = num05 * num06;
 
-//table/sub-table js
-function toggleTable(row) {
-  var subTable = row.nextElementSibling;
-  if (subTable.style.display === "none") {
-    subTable.style.display = "table-row";
-  } else {
-    subTable.style.display = "none";
-  }
+  document.getElementById('resultdatabase').textContent = `Services: ${databaseProduct}`;
 }
+
+
+//Workstations Calcs
+const num07 = parseInt();
+const num08 = parseInt();
+const workstationProduct = num07 + num08;
+
+console.log(`The product of ${num07} and ${num08} is ${workstationProduct}`);
+
+function calculateProduct() {
+  const num07 = parseInt(document.getElementById('num07').value);
+  const num08 = parseInt(document.getElementById('num08').value);
+
+  const workstationProduct = num07 * num08;
+
+  document.getElementById('resultWorkstation').textContent = `Services: ${workstationProduct}`;
+}
+
+
+//24-Port Switch Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
+//48-Port Switch Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
+//Routers Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
+//Access Points Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
+//Websites Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
+//Other Node Calcs
+const num01 = parseInt();
+const num02 = parseInt();
+const linuxServerProduct = num01 + num02;
+
+console.log(`The product of ${num01} and ${num02} is ${linuxServerProduct}`);
+
+function calculateProduct() {
+  const num01 = parseInt(document.getElementById('num01').value);
+  const num02 = parseInt(document.getElementById('num02').value);
+
+  const linuxServerProduct = num01 * num02;
+
+  document.getElementById('resultLinuxServer').textContent = `Services: ${linuxServerProduct}`;
+}
+
+
